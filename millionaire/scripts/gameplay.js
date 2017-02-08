c56e7d8f039df49d9bf5b1e81339d7c6fcd3a8d9
Alastair Stanley <alastairstanley@ntlworld.com>
2017-02-08 15:20:51 +0000

    Force wait for question to load

--- millionaire/scripts/gameplay.js
+++ millionaire/scripts/gameplay.js
@@ -1,6 +1,5 @@
 var questionNum, timeToAnswer, selected = '', winnings, timeSaved,
-    version, money, ladderHTML, currentMessage,
-    usedFiftyFifty = 0, usedPhoneAFriend = 0, usedAskAudience = 0;
+    version, money, ladderHTML, currentMessage, checking;
 
 function reset( whichVersion ) {
   questionNum = 1;
@@ -21,9 +20,9 @@ function reset( whichVersion ) {
     $(this).attr('src','images/lifeline-'+$(this).attr('id')+'.png');
   }).click(function(){ confirmLifeline($(this).attr('id')); });
 
-  if (version == 'traditional') money = [0,'100','200','300','500','1,000','2,000','4,000','8,000','16,000','32,000','64,000','125,000','250,000','500,000','1 MILLION'];
+  if (version == 'traditional') money = ['0','100','200','300','500','1,000','2,000','4,000','8,000','16,000','32,000','64,000','125,000','250,000','500,000','1 MILLION'];
 
-  if (version == "modern") money = [0,'500','1,000','2,000','5,000','10,000','20,000','50,000','75,000','150,000','250,000','500,000','1 MILLION'];
+  if (version == "modern") money = ['0','500','1,000','2,000','5,000','10,000','20,000','50,000','75,000','150,000','250,000','500,000','1 MILLION'];
 
   ladderHTML = ''
   for (var i=money.length-1; i; i--)
@@ -48,12 +47,17 @@ function areYouReady( stage ) {
   $('#confirm').css('visibility','hidden');
   $('input').css('visibility','hidden');
 
-  $('#message-board').css('visibility','visible');
+  checking = setInterval(function(){
+    if (getQuestionSuccess == true) {
+      clearInterval(checking);
+      $('#message-board').css('visibility','visible');
+    }
+  },100);
 }
 
 
 function areYouSure() {
-  $('#message-text').html((money[questionNum-1] > 0 || currentMessage == 'walk?')? 'Walk away with £'+(money[questionNum-1]||0)+'?':'Are you sure?');
+  $('#message-text').html((money[questionNum-1].length > 1 || currentMessage == 'walk?')? 'Walk away with £'+(money[questionNum-1])+'?':'Are you sure?');
   $('#quit').html('Yes').css('visibility','inherit');
   $('#next-question').html('No').css('visibility','inherit');
   $('#confirm').css('visibility','hidden');
@@ -65,6 +69,7 @@ function areYouSure() {
 
 function readyQuestion( k ) {
   selected = '';
+  remove = [];
   $('#final').attr('class','inactive');
   $('.answer-text').parent().attr('class','');
   if (k>1) {
@@ -90,6 +95,7 @@ function confirmLifeline( name ) {
     case '5050': $('#message-text').html('Use the 50-50 lifeline?<br/>This will remove two incorrect answers'); break;
     case 'paf': $('#message-text').html('Phone a friend?<br/>They may be able to help with the question'); break;
     case 'ata': $('#message-text').html('Ask the audience?<br/>You will be shown how many would choose each answer'); break;
+    case 'switch': $('#message-text').html('Use the Switch lifeline?<br/>The question will be replaced by a new one'); break;
     case 'help': $('#message-text').html('You can use each lifeline only once per game<br/><br/>Click on the icons to find out what they do');
       $('#confirm').css('visibility','inherit');
       $('#quit').css('visibility','hidden');
@@ -113,24 +119,32 @@ function questionSuccess() {
   if (version == 'traditional') {
     if (questionNum == 5) winnings = 1000;
     if (questionNum == 10) winnings = 32000;
+    if (questionNum == 15) { millionaire(); return; }
   } else {
     if (questionNum == 2) winnings = 1000;
     if (questionNum == 7) winnings = 50000;
+    if (questionNum == 12) { millionaire(); return; }
   }
   questionNum++
   timeSaved += currentTime;
-  readyQuestion(questionNum);
   areYouReady(questionNum);
 }
 
-
 function questionFailure() {
+  if (highScore()) { newHighScore(); return; }
   $('#message-text').html('Game over!<br/><br/>You go home with £'+winnings);
   $('#quit').css('visibility','hidden');
   $('#next-question').css('visibility','hidden');
-  highScore()? newHighScore() : $('#confirm').html('Back to Menu').css('visibility','inherit');
+  $('#confirm').html('Back to Menu').css('visibility','inherit');
   $('#message-board').css('visibility','visible');
-  $('input').css('visibility','hidden');
+}
+
+
+function millionaire() {
+  questionNum = 13;
+  celebrate();
+  currentMessage = 'winner';
+  setTimeout(newHighScore,3000);
 }
 
 
@@ -138,6 +152,7 @@ $('#quit').click(function(){
   if (currentMessage == '5050') fiftyFifty();
   else if (currentMessage == 'paf') phoneAFriend();
   else if (currentMessage == 'ata') askAudience();
+  else if (currentMessage == 'switch') switchQuestion();
   else if (currentMessage != 'ready?' && highScore())
     newHighScore();
   else if (currentMessage != 'ready?' || questionNum == 1) {
@@ -153,7 +168,7 @@ $('#next-question').click(function(){
       $('#next-question').css('visibility','hidden');
       $('#confirm').html('Ok').css('visibility','inherit');
       $('#switch').css('visibility','visible');
-      currentMessage = '';
+      currentMessage = 'new life';
     }
     else { $('#message-board').css('visibility','hidden');
       startQuestion(); }
@@ -161,11 +176,13 @@ $('#next-question').click(function(){
   else $('#message-board').css('visibility','hidden');
 });
 $('#confirm').click(function(){
-  if (currentMessage == 'phone') startTimer();
-  if (currentMessage == 'help' || currentMessage == 'phone') { $('#message-board').css('visibility','hidden'); return; }
+  if (currentMessage == 'new life') startQuestion();
+  if (currentMessage == 'info') startTimer();
+  if (currentMessage == 'help' || currentMessage == 'info' || currentMessage == 'new life') { $('#message-board').css('visibility','hidden'); return; }
   if ($(this).attr('class') == 'inactive') return;
   if (currentMessage == 'newHighScore') updateHighScores();
   $('#home').css({animation:'inLeft 1s','animation-fill-mode':'forwards'});
+  stopCelebrating();
 });
 
 
