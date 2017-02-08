102ab7dc8f09b847fa133333b2a8d52fc242d76d
Alastair Stanley <alastairstanley@ntlworld.com>
2017-02-08 15:21:45 +0000

    Achievable high score

--- millionaire/scripts/high-scores.js
+++ millionaire/scripts/high-scores.js
@@ -1,15 +1,14 @@
-var highScores = {'traditional':[['TL27',1000000,0,42],['BOB',125000,3,212],['MARIE',64000,0,468],['AMY',64000,2,227],['JAKE',32000,3,84]],
-            'modern':[['ALICE',250000,2,130],['JAMES',150000,3,46],['SALLY',75000,2,141],['OLAF',75000,4,83],['TOM',50000,2,11]]},
+var highScores = {'traditional':[['TL27',1000000,1,42],['BOB',125000,3,212],['MARIE',64000,0,468],['AMY',64000,2,227],['JAKE',32000,3,84]],
+            'modern':[['ALICE',250000,2,130],['JAMES',150000,3,46],['SALLY',75000,2,141],['OLAF',75000,4,83],['TOM',1000,2,11]]},
     lifelinesUsed, worstEntry, currentEntry, highScoresHTML, minutes, char, name;
 highScores = JSON.parse(localStorage.getItem('high-scores')) || highScores;
 displayHighScores();
 
 function highScore() {
   worstEntry = highScores[version][4];
-  console.log(worstEntry);
-  if (money[questionNum-1] > worstEntry[1]) return true;
-  if (money[questionNum-1] < worstEntry[1]) return false;
-  lifelinesUsed = usedFiftyFifty + usedPhoneAFriend + usedAskAudience;
+  if (Number(money[questionNum-1].replace(',','')) > worstEntry[1]) return true;
+  if (Number(money[questionNum-1].replace(',','')) < worstEntry[1]) return false;
+  lifelinesUsed = usedFiftyFifty + usedPhoneAFriend + usedAskAudience + usedSwitch;
   if (lifelinesUsed < worstEntry[2]) return true;
   if (lifelinesUsed > worstEntry[2]) return false;
   if (timeSaved >= worstEntry[3]) return true;
@@ -43,8 +42,9 @@ function time( seconds ) {
 
 function newHighScore() {
   stopTimer();
+  if (currentMessage == 'winner') $('#message-text').html('You won £1MILLION<br/><br/>Enter your name:');
+  else $('#message-text').html('New high score!<br/><br/>Enter your name:');
   currentMessage = 'newHighScore';
-  $('#message-text').html('New high score!<br/><br/>Enter your name:');
   $('input').val('').css('visibility','visible');
   $('#confirm').html('Done').attr('class','inactive').css('visibility','inherit');
   $('#quit').css('visibility','hidden');
@@ -53,7 +53,10 @@ function newHighScore() {
 }
 
 function updateHighScores() {
-  currentEntry = [$('input').val(),Number(money[questionNum-1].replace(',','')),lifelinesUsed,timeSaved];
+  lifelinesUsed = usedFiftyFifty+usedPhoneAFriend+usedAskAudience+usedSwitch;
+  if (questionNum > 12)
+    currentEntry = [$('input').val(),1000000,lifelinesUsed,timeSaved];
+  else currentEntry = [$('input').val(),Number(money[questionNum-1].replace(',','')),lifelinesUsed,timeSaved];
   highScores[version][4] = currentEntry;
   highScores[version].sort(function(a,b){
     if (a[1] > b[1]) return -1;  if (a[1] < b[1]) return 1;
@@ -69,7 +72,7 @@ $('input').keydown(function(e) {
   char = e.which;
   if (char != 8) e.preventDefault();
   else if ($('input').val().length == 1) $('#confirm').attr('class','inactive');
-  if (char > 64 && char < 91 && $('input').val().length < 4) {
+  if (char > 64 && char < 91 && $('input').val().length < 5) {
     $('input').val($('input').val()+String.fromCharCode(char));
     $('#confirm').attr('class','active');
   }
