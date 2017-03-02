4d7ebac824456e86ce218c9ea5667b2c9a94f1b3
Alastair Stanley <alastairstanley@ntlworld.com>
2017-03-02 16:23:48 +0000

    comments in life

--- life/draw.js
+++ life/draw.js
@@ -1,4 +1,4 @@
-var gridSize = 200, pix = 1000/gridSize, mouse, pixScale = 20, gridOn = 1;
+var gridSize = 200, pix = 1000/gridSize, mouse, pixScale = 20, gridOn = 1, index;
     canvas = document.getElementById("board"), ctx = canvas.getContext("2d"),
     grid = document.getElementById("grid"), ctxG = grid.getContext("2d"),
     overlay = document.getElementById("overlay"), ctxO = overlay.getContext("2d"),
@@ -11,40 +11,69 @@ document.getElementById("toggleGrid").style.backgroundColor = "#BBB";
 ctxO.fillStyle = "#AAA";
 ctxO.strokeStyle = "#AAA";
 
+
+
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
+// functions to map points to and from their positions in a zoomed-in window
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
 function zX( x ) { return (x-500)*pixW/pix+500; }
 function zY( y ) { return (y-500)*pixH/pix+500; }
 function z_X( x ) { return (x-500)*pix/pixW+500; }
 function z_Y( y ) { return (y-500)*pix/pixH+500; }
 
+
+
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
+// listeners to draw or erase living cells on the grid
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
 overlay.addEventListener("mousedown",function(e){e.preventDefault(); adding(e);});
 overlay.addEventListener("mousemove",function(e){adding(e);});
 
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
+// function to create or kill a living cell
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
 function adding(e) {
+  // find the index of the cell under the mouse pointer
   getCell(e);
-  if ((e.which == 1) && !alive.includes(cellX+gridSize*cellY)) {
+  index = cellX+gridSize*cellY;
+
+  // detect left mouse-click (and drag): create living cell (unless already alive)
+  if ((e.which == 1) && !alive.includes(index)) {
+    // stop continuous play when grid is edited
     stop();
-    alive.push(cellX+gridSize*cellY);
+
+    alive.push(index);
     updateShapes();
-  } else if ((e.which == 3) && !living && alive.includes(cellX+gridSize*cellY)) {
-    alive.splice(alive.indexOf(cellX+gridSize*cellY),1);
+
+  // detect right mouse-click (and drag): kill cell (unless already dead)
+  } else if ((e.which == 3) && !living && alive.includes(index)) {
+    alive.splice(alive.indexOf(index),1);
     updateShapes();
   }
 }
 
-overlay.addEventListener("mousewheel",function(e){updateZoom(e);});
-overlay.addEventListener("DOMMouseScroll",function(e){updateZoom(e);});
-window.addEventListener("resize",function(){updateZoom(0);});
 
+
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
+// function to redraw the cells on the grid using the marching squares algorithm
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
 function updateShapes() {
+  // clear the existing shapes
   ctx.clearRect(0,0,1000,1000);
+
+  // iterate through all squares, recording which corners are 'alive'
   for (var i=0; i<gridSize*gridSize; i++) {
     cornerPoints = [];
     if (alive.includes(i)) cornerPoints.push(0);
     if (alive.includes(i+1) && (i+1)%gridSize) cornerPoints.push(1);
     if (alive.includes(i+gridSize+1) && (i+1)%gridSize) cornerPoints.push(2);
     if (alive.includes(i+gridSize)) cornerPoints.push(3);
+
+    // draw the correct shape in the square
     drawShape(cornerPoints,i%gridSize*pix,Math.floor(i/gridSize)*pix);
   }
+
+  // round all corners by drawing circles on top of living corner points
   for (var i in alive) {
     i = alive[i];
     ctx.beginPath();
@@ -53,9 +82,15 @@ function updateShapes() {
   }
 }
 
+
+
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
+// function to draw the correct marching squares shape in a square based on its living corner points
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
 function drawShape( points, x, y ) {
   ctx.beginPath();
   ctx.moveTo(zX(x+(1-Math.abs(points[0]-2)/2)*pix),zY(y+Math.abs(points[0]-1)/2*pix));
+  // iterate through living corner points, connecting midpoint of anticlockwise edge –– corner –– midpoint of clockwise edge to create the correct shape
   for (var i in points) {
     i = points[i];
     ctx.lineTo(zX(x+(1-Math.abs(i-2)/2)*pix),zY(y+Math.abs(i-1)/2*pix));
@@ -65,6 +100,11 @@ function drawShape( points, x, y ) {
   ctx.fill();
 }
 
+
+
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
+// function to find the index of the cell under the mouse pointer
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
 function getCell(e) {
   rect = canvas.getBoundingClientRect();
   mouse = {'x':(e.clientX-rect.left)*canvas.width/rect.width,
@@ -78,9 +118,16 @@ function getCell(e) {
   ctxO.fill();
 }
 
+
+
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
+// function to redraw the grid lines
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
 function updateGrid() {
   ctxG.clearRect(0,0,1000,1000);
   if (!gridOn) return;
+
+  // vertical
   ctxG.lineWidth = pixW/5;
   for (var i=500-gridSize*pixW/2; i<500+gridSize*pixW/2; i+=pixW) {
     ctxG.beginPath();
@@ -88,6 +135,8 @@ function updateGrid() {
     ctxG.lineTo(i,1000);
     ctxG.stroke();
   }
+
+  // horizontal
   ctxG.lineWidth = pixH/5;
   for (var j=500-gridSize*pixH/2; j<500+gridSize*pixH/2; j+=pixH) {
     ctxG.beginPath();
@@ -97,6 +146,18 @@ function updateGrid() {
   }
 }
 
+
+
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
+// listeners to resize the grid
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
+overlay.addEventListener("mousewheel",function(e){updateZoom(e);});
+overlay.addEventListener("DOMMouseScroll",function(e){updateZoom(e);});
+window.addEventListener("resize",function(){updateZoom(0);});
+
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
+// function to zoom in and out on the grid
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
 function updateZoom(e) {
   rect = canvas.getBoundingClientRect();
   pixScale = Math.min(Math.max(pixScale+e.wheelDelta/10 || pixScale, rect.width/gridSize,rect.height/gridSize), 100);
@@ -107,6 +168,11 @@ function updateZoom(e) {
   updateGrid();
 }
 
+
+
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
+// function to show or hide the grid lines
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
 function toggleGrid() {
   gridOn = (gridOn)? false : true;
   document.getElementById("toggleGrid").style.backgroundColor = (gridOn)? "#BBB":"white";
--- life/life.js
+++ life/life.js
@@ -1,45 +1,84 @@
 var fps = 10, alive = [], candidates = {}, newAlive = [], living, timeouts = [];
 
+
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
+// function to do one iteration of the game of life
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
 function updateBoard() {
+
+  // check if game has been turned off
   if (living)
+    // if not, do the next iteration after timeout according to fps
     timeouts.push(setTimeout(function(){
       requestAnimationFrame(updateBoard());
     },1000/fps));
 
+
+  // initialise object to hold possible state-change cells
   candidates = {};
+
+  // iterate through living cells, incrementing the neighbour count of all adjacent cells
   for (var i in alive) {
     i = Number(alive[i]);
+
+    // increment neighbour count for cells above & below the current living cell
     candidates[i-gridSize] = ++candidates[i-gridSize]||1;
     candidates[i+gridSize] = ++candidates[i+gridSize]||1;
+
+    // increment neighbour count for cells left, above-left & below-left of the current living cell, unless the current cell is on the left-hand boundary
+    // overflowing the top or bottom boudaries is ok at this stage, but left- and right-hand boundaries wrap around
     if (i%gridSize > 0) {
       candidates[i-gridSize-1] = ++candidates[i-gridSize-1]||1;
       candidates[i-1]   = ++candidates[i-1]||1;
       candidates[i+gridSize-1]  = ++candidates[i+gridSize-1]||1;
     }
+
+    // increment neighbour count for cells right, above-right & below-right of the current living cell, unless the current cell is on the right-hand boundary
     if (i%gridSize < gridSize-1) {
       candidates[i-gridSize+1]  = ++candidates[i-gridSize+1]||1;
       candidates[i+1]   = ++candidates[i+1]||1;
       candidates[i+gridSize+1] = ++candidates[i+gridSize+1]||1;
     }
   }
+
+  // initialise array to hold the confirmed state-change cells
   newAlive = [];
+
   for (var j in candidates) {
     j = Number(j);
+    // check that the cell index is in range (deals with top/bottom grid overflow)
     if (0 <= j && j < gridSize*gridSize)
+      // enforces the rules of the game of life:
       switch (candidates[j]) {
         case 2: if (alive.includes(j)) newAlive.push(j); break;
         case 3: newAlive.push(j); break;
       }
-  } alive = newAlive;
+  }
+
+  // update record of living cells
+  alive = newAlive;
+  // then draw them on the webpage
   updateShapes();
 }
 
+
+
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
+// function to start continuous play
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
 function go() {
+  // (do nothing if already running)
   if (living) return
+
   living = true;
   updateBoard();
 }
 
+
+
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
+// function to stop continuous play
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
 function stop() {
   living = false;
   for (var i in timeouts) clearTimeout(timeouts[i]);
